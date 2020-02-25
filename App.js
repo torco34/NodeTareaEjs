b7760d1b8220c5cba23f0c2bf0242aa3e0181397
torco34 <torcoromaarias@gimail.com>
2020-02-25 16:04:39 -0500

    Tabla De Tarea

---
+++ App.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan')
+const path = require('path');
+const bodyParser = require('body-parser')
+const app = express();
+
+
+app.set('port', process.env.PORT || 3000);
+
+//carpeta views
+
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'ejs' )
+
+
+//aqui va morgan
+
+app.use(morgan('dev'));
+
+//body json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+ 
+ //la rutas
+app.get('/', (req, res) =>{
+	res.render('index');
+})
+app.listen(3000, () => {
+	console.log(`cadena de texto ${3000}`)
+}) 
